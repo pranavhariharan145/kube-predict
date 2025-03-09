d6301921db2a44663c7bd9fd402a9ea0d8f472ab
Pranav <pranavhariharan145@gmail.com>
2025-03-09 18:47:07 +0530

    training and testing sets

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from tensorflow.keras.layers import LSTM, Dense
 
 # Load the data
 data = pd.read_csv('kubernetes_performance_metrics_dataset.csv')
-print(data.head())
+
 
 
 # Data Seperation 
@@ -30,6 +30,14 @@ print(data.head())
 y = data['pod_lifetime_seconds']
 X = data.drop(['pod_lifetime_seconds'], axis=1)
 
-print(X.head())
-print(y.head())
+
+
+
+# Split the dta into training and testing sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+ 
 
