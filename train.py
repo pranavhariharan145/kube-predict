a75f0160ea82ada84cccc4f25b3fad33cede770e
Pranav <pranavhariharan145@gmail.com>
2025-03-09 18:38:13 +0530

    data seperation x val and y val

--- train.py
+++ train.py
@@ -12,3 +12,24 @@ data = pd.read_csv('kubernetes_performance_metrics_dataset.csv')
 print(data.head())
 
 
+# Data Seperation 
+# Possible X values: timestamp (Possibly useful if you're doing time-series analysis)
+# pod_name (Might be categorical)
+# namespace (Might be categorical)
+# cpu_allocation_efficiency
+# memory_allocation_efficiency
+# disk_io
+# network_latency
+# node_temperature
+# node_cpu_usage
+# node_memory_usage
+# scaling_event (Might be categorical)
+
+# Possible Y values: pod_lifetime_seconds
+
+y = data['pod_lifetime_seconds']
+X = data.drop(['pod_lifetime_seconds'], axis=1)
+
+print(X.head())
+print(y.head())
+
