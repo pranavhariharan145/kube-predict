969f494cbabddc41fa89aba85f909d275c9da9e5
Pranav <pranavhariharan145@gmail.com>
2025-03-09 18:29:15 +0530

    loadn the data

--- train.py
+++ train.py
@@ -4,3 +4,11 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import IsolationForest
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
+
+# Goal::  Predicting Pod Lifetime
+
+# Load the data
+data = pd.read_csv('kubernetes_performance_metrics_dataset.csv')
+print(data.head())
+
+
