4377decb486e401393e047d31dbb11f14c8f34d3
Pranav <pranavhariharan145@gmail.com>
2025-03-09 18:21:20 +0530

    add: perforamnce dataset

---
+++ train.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.ensemble import IsolationForest
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
