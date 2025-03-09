a219d363adf1bf7f18b3dffccbbea7cf492a422c
Pranav <pranavhariharan145@gmail.com>
2025-03-09 19:31:29 +0530

    using randomforest regressor for prediction

--- train.py
+++ train.py
@@ -10,7 +10,9 @@ from tensorflow.keras.layers import LSTM, Dense
 # Load the data
 data = pd.read_csv('kubernetes_performance_metrics_dataset.csv')
 
-
+data["timestamp"] = pd.to_datetime(data["timestamp"], errors="coerce")  # Convert to datetime
+data["timestamp"] = data["timestamp"].astype("int64") // 10**9  # Convert to Unix timestamp (seconds)
+data = data.drop(["pod_name", "namespace", "event_type", "event_message", "scaling_event"], axis=1)
 
 # Data Seperation 
 # Possible X values: timestamp (Possibly useful if you're doing time-series analysis)
@@ -39,5 +41,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
- 
+
+# Train an Isolation Forest model to detect outliers
+from sklearn.ensemble import RandomForestRegressor
+
+rf = RandomForestRegressor(max_depth=2, random_state=100)
+rf.fit(X_train, y_train)
+
+y_random_forest_train = rf.predict(X_train)
+y_random_forest_test = rf.predict(X_test)
+
+# Should print the number of trees (default is 100)
+print(rf.n_estimators)
 
